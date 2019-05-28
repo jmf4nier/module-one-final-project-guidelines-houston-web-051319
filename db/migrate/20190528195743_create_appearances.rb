bb4a69e3a74a20b954262d8973c2d412d6d6cb79
Jason Fournier <jmf4nier@hotmail.com>
2019-05-28 15:14:46 -0500

    created appearances/events tables and migrated them

--- db/migrate/20190528195743_create_appearances.rb
+++ db/migrate/20190528195743_create_appearances.rb
@@ -1,4 +1,8 @@
 class CreateAppearances < ActiveRecord::Migration[5.0]
   def change
+    create_table :appearances do |t|
+      t.integer :event_id
+      t.integer :artist_id
+    end
   end
 end
