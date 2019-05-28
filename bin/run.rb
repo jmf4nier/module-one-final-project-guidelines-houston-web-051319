299017a51573d17b82803eaf7a3e24a86984ac57
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-28 17:52:09 -0500

    Very early stage of TTY-prompt

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,8 @@
 require_relative '../config/environment'
+prompt = TTY::Prompt.new
 
+
+prompt.select("Welcome to Ticket Master! What would you like to do?", User.situation)
 
 
 puts "HELLO WORLD"
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
     has_many :tickets, dependent: :destroy
     has_many :events, through: :tickets
+
+    @@situation = ["Sign up", "Login"]
+
+    def self.situation
+        @@situation
+    end
+
 end
