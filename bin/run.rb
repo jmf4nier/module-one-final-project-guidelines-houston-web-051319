2a49e549e2b6bb97da667ad4a1dc6659ec3fbb03
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-28 22:25:25 -0500

    Developed simple authentication menu

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,50 @@
 require_relative '../config/environment'
-prompt = TTY::Prompt.new
+$prompt = TTY::Prompt.new
 
+$user = nil
 
-prompt.select("Welcome to Ticket Master! What would you like to do?", User.situation)
+def situation_selection
+    $prompt.select("Welcome to Ticket Master! What would you like to do?") do |menu|
+        menu.choice 'Sign up'
+        menu.choice 'Login'
+    end
+end
 
+def autheticate_user_screen
+    while($user == nil)
+        selection = situation_selection
+        if(selection == "Sign up")#Create new user and save to data base. Then enter logged in screen
+            puts "Please enter a name"
+            name = gets.chomp
+            new_user = User.new(name: name)
+            new_user.save
+            $user = new_user
+        else#User selected Login. 
+            loop do 
+                puts "Please enter a name"
+                name = gets.chomp
+                break if name == "back"
+                authenticated_user = User.all.find_by(name: name)
+                if(authenticated_user != nil)
+                    $user = authenticated_user
+                    break
+                end
+                puts "Incorrect Name. Try again" #Name for now. In the future, program may be updated to contain username/password
+            end     
+        end
+    end
+    #logged_in_screen
+end
 
-puts "HELLO WORLD"
+def logged_in_screen
+    $prompt.select("What would you like to do?") do |menu|
+        menu.choice 'View all purchased tickets'
+        menu.choice 'View events in a specified city'
+    end
+end
+
+#===========================================
+#MAIN METHOD
+ autheticate_user_screen
+ logged_in_screen
+ puts "Working Program!!"
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,11 +1,4 @@
 class User < ActiveRecord::Base
     has_many :tickets, dependent: :destroy
     has_many :events, through: :tickets
-
-    @@situation = ["Sign up", "Login"]
-
-    def self.situation
-        @@situation
-    end
-
 end
