336dc16c6ca69be1f20f2cf122041637b7620736
Jason Fournier <jmf4nier@hotmail.com>
2019-05-29 10:17:25 -0500

    changed seed file to include city state venue_name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-Event.create(name: "Fyre", date: 10/27/2019, city: "Houston", state: "Texas", venue: "Rose Garden")
-Event.create(name: "Lolapalooza", date: 6/27/2019, city: "Houston", state: "Texas", venue: "Minute Maid")
-Event.create(name: "Woodstock", date: 8/27/2019, city: "San Antonio", state: "Texas", venue: "Alamo")
-Event.create(name: "Buzz Fest", date: 10/15/2020, city: "Austin", state: "Texas", venue: "Lady Bird Lake")
-Event.create(name: "Burning Man", date: 10/27/2019, city: "Dallas", state: "Texas", venue: "Reliant")
+Event.create(name: "Fyre", date: 10/27/2019, city: "Houston", state: "Texas", venue_name: "Rose Garden")
+Event.create(name: "Lolapalooza", date: 6/27/2019, city: "Houston", state: "Texas", venue_name: "Minute Maid")
+Event.create(name: "Woodstock", date: 8/27/2019, city: "San Antonio", state: "Texas", venue_name: "Alamo")
+Event.create(name: "Buzz Fest", date: 10/15/2020, city: "Austin", state: "Texas", venue_name: "Lady Bird Lake")
+Event.create(name: "Burning Man", date: 10/27/2019, city: "Dallas", state: "Texas", venue_name: "Reliant")
 users = ["Stefan", "Kurtis", "Carlos", "Feven"]
 users.each do |name|
     User.create(name: name)
