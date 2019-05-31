e57ce0818af23dfd3a33214f3311b36a03aa6679
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-31 11:38:48 -0500

    updated get_more_info method and created get_string_artist_names method

--- lib/models/artist.rb
+++ lib/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ActiveRecord::Base
     has_many :appearances, dependent: :destroy
     has_many :events, through: :appearances
+
+    def print_name
+        print self.name
+    end
 end
