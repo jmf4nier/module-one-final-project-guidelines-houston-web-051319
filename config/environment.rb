39deeb3426f647cb205ed1a1d6ada159f5019902
Jason Fournier <jmf4nier@hotmail.com>
2019-06-10 09:05:33 -0500

    new branch

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ require 'rest-client'
 require 'json'
 Bundler.require
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-# ActiveRecord::Base.logger = nil
+ ActiveRecord::Base.logger = nil
 require_all 'lib'
